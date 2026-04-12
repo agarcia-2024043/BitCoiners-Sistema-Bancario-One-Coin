704b98e168fd454caf0ec8b91a5058bd31d9c646
agarcia-2024043 <agarcia-2024043@kinal.edu.gt>
2026-04-11 18:50:56 -0600

    feat: Agregar Interfaces a Sistema Bancario One Coin

---
+++ Sistema Bancario One Coin/authentication-service/auth-service/src/AuthService.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+using AuthService.Domain.Entities;
+
+namespace AuthService.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<bool> ExistsAsync(string email); 
+
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByVerificationTokenAsync(string token);
+    Task<User?> GetByResetTokenAsync(string token);
+    Task<User?> GetByIdAsync(Guid id);
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+    Task DeleteAsync(Guid id);
+
+    // Nuevo: buscar un rol por nombre para asignarlo al registrar
+    Task<Role?> GetRoleByNameAsync(string roleName);
+}
