21d7e89f2e70f0bcae17e9c9dac8d117d8484ad1
agarcia-2024043 <agarcia-2024043@kinal.edu.gt>
2026-04-26 00:24:43 -0600

    docs: Habilitar la generación de documentación XML para Swagger

--- Sistema Bancario One Coin/authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ Sistema Bancario One Coin/authentication-service/auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b5debc85454785bad4b5bcc5b73d99e097683a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c40b29317a2ef8f207ec36c0b241ae0df05f2401")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
