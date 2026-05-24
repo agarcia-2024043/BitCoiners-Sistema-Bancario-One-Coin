280518cf7dff121d8018a841bdfcf3d02face2dc
agarcia-2024043 <agarcia-2024043@kinal.edu.gt>
2026-05-24 10:43:07 -0600

    feat: Agregar Dtos a Sistema Bancario One Coin

--- Sistema Bancario One Coin/authentication-service/auth-service/src/AuthService.Application/DTOs/UserDetailsDto.cs
+++ Sistema Bancario One Coin/authentication-service/auth-service/src/AuthService.Application/DTOs/UserDetailsDto.cs
@@ -6,4 +6,5 @@ public class UserDetailsDto
     public string Username { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Role { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
 }
